8011b53258c109c676f47ade4c26fbce5cd105a2
udosinachi <udosipat@gmail.com>
2021-01-14 21:55:45 +0100

    Installed outdated cb

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { handleChange } from './actions/promoCodeAction'
 
 const Grid = styled.div`
   width: 350px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   padding: 1em;
 `
 const Wrapper = styled.div`
